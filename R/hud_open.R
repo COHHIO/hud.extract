2c1c5d9bbe6c7ca1e5f845994564f2f651efc134
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-15 06:44:18 -0500

    add `hud_export_extract` from `clarity.automation`

--- R/hud_open.R
+++ R/hud_open.R
@@ -38,3 +38,38 @@ open_pdfs = function(type = c("csv_specs", "data_dictionarys", "data_standards",
   cli::cli_alert_success(paste0("Opening ", f))
   f
 }
+
+#' @title Extract a previously downloaded HUD Export archive
+#'
+#' @param browser_dl_folder \code{(character)} path to the browser's download folder
+#' @param extract_path \code{(character)} path to the folder where the archive is to be extracted
+#' @param delete_archive \code{(logical)} Delete the archive after extracting?
+#' @param moment \code{(POSIXct/Date)} The time point which the archive creation time should be greater than to ensure it's recent.
+#' @param wait \code{(Duration)} to wait for the file to appear in the download directory. Relevant when using browser automation.
+#' @return \code{(logical)} as to whether the extraction was successful
+#' @export
+
+hud_export_extract <- function(browser_dl_folder = "~/../Downloads", extract_path = "data", delete_archive = TRUE, moment = Sys.Date(), wait = lubridate::minutes(2)) {
+  downloads <- path.expand(browser_dl_folder)
+  dls <- list.files(downloads, full.names = TRUE, pattern = "^hudx")
+  dl_times <- do.call(c, purrr::map(dls, ~file.info(.x)$mtime))
+  if (!UU::is_legit(dl_times))
+    cli::cli_alert(paste0("No HUD Export found in ", path.expand(downloads), " waiting ", wait))
+  wait = lubridate::now() + wait
+  .recent <- dl_times > moment
+  while (!any(.recent) && Sys.time() < wait) {
+    Sys.sleep(5)
+    dls <- list.files(downloads, full.names = TRUE, pattern = "^hudx")
+    dl_times <- do.call(c, purrr::map(dls, ~file.info(.x)$mtime))
+    .recent <- dl_times > moment
+  }
+
+  if (any(.recent)) {
+    f <- dls[.recent]
+    archive::archive_extract(f, path.expand(extract_path))
+  } else
+    cli::cli_alert("No HUD Export found in ", path.expand(downloads), " with creation time greater than ", moment)
+
+  if (delete_archive)
+    file.remove(dls[dl_times > moment])
+}
